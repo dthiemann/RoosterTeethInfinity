d9b978348040079b644e4bb084d492e8db96b231
Dylan Thiemann <dylanthiemann@gmail.com>
2015-01-14 16:16:31 -0600

    Removed old folder

---
+++ RoosterTeethInfinity/RoosterTeethInfinity.Shared/YouTubeVideo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoosterTeethInfinity
+{
+    public class YouTubeVideo
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public DateTime PubDate { get; set; }
+        public Uri YouTubeLink { get; set; }
+        public Uri VideoLink { get; set; }
+        public Uri Thumbnail { get; set; }
+    }
+}
