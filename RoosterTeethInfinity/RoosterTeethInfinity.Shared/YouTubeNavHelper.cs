9e8ddb044862ec79bd1089fb7607deabf17a49c8
Dylan Thiemann <dylanthiemann@gmail.com>
2015-01-15 13:06:48 -0600

    Added private variables for YouTubeNavHelper

--- RoosterTeethInfinity/RoosterTeethInfinity.Shared/YouTubeNavHelper.cs
+++ RoosterTeethInfinity/RoosterTeethInfinity.Shared/YouTubeNavHelper.cs
@@ -6,5 +6,9 @@ namespace RoosterTeethInfinity
 {
     class YouTubeNavHelper
     {
+        private string baseUrl { get; set; }
+        private string indexUrl { get; set; }
+        private string resultUrl { get; set; }
+        private string channelName { get; set; }
     }
 }
